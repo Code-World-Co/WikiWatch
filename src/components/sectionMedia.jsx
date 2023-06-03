f05d53d88b69052444d9cb3710175a4def3fa528
Darkin03 <esnaiderortega@unisinu.edu.co>
2023-06-02 22:47:17 -0500

    Add slider for MediaCards

--- src/components/sectionMedia.jsx
+++ src/components/sectionMedia.jsx
@@ -1,74 +1,97 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-//import components
 import { MediaCard } from "./mediaCard";
-//import style
 import "../style/MediaCard.css";
-//import icons
-import { CiGrid41} from "react-icons/ci";
+import { CiGrid41 } from "react-icons/ci";
 import { MdOutlineKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
 export function SectionMedia(props) {
-    const [media, setMedia] = useState([])
-    const [topleft, setTopleft] = useState(0)
-    const [topright, setTopright] = useState(5)
+  const [media, setMedia] = useState([]);
+  const [topleft, setTopleft] = useState(0);
+  const [topright, setTopright] = useState(5);
+  const [cardsToShow, setCardsToShow] = useState(5);
 
-    useEffect(() => {
-        setMedia(props.media.slice(topleft, topright))
-    }, [topleft, topright, props.media])
+  useEffect(() => {
+    setMedia(props.media.slice(topleft, topright));
+  }, [topleft, topright, props.media]);
 
-    const clickLeft = () => {
-        if (topleft > 0) {
-            setTopleft(topleft - 5)
-            setTopright(topright - 5)
-        }
+  useEffect(() => {
+    function handleResize() {
+      const screenWidth = window.innerWidth;
+      let newCardsToShow = 5;
+
+      if (screenWidth < 768) {
+        newCardsToShow = 1;
+      } else if (screenWidth < 1024) {
+        newCardsToShow = 3;
+      } else if (screenWidth < 1200) {
+        newCardsToShow = 4;
+      }
+
+      setCardsToShow(newCardsToShow);
+      setTopright(topleft + newCardsToShow);
     }
 
-    const clickRight = () => {
-        if (topright < props.media.length) {
-            setTopleft(topleft + 5)
-            setTopright(topright + 5)
-        }
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [topleft]);
+
+  const clickLeft = () => {
+    if (topleft > 0) {
+      setTopleft(topleft - cardsToShow);
+      setTopright(topright - cardsToShow);
     }
+  };
 
-
-    const onclick = () => {
-        window.scrollTo(0, 0)
+  const clickRight = () => {
+    if (topright < props.media.length) {
+      setTopleft(topleft + cardsToShow);
+      setTopright(topright + cardsToShow);
     }
+  };
 
-    return (
-        <section className="sectionMedia">
-            <header>
-                <h1 className="title">{props.title}</h1>
-            </header>
+  const onclick = () => {
+    window.scrollTo(0, 0);
+  };
 
-            
-            <div className="linkContainer">
-                <Link className="link" to="/all"><CiGrid41/>All</Link>
-            </div>
-                
-            <section className="sectionMediaContent">
-                <div className="mediaContainer">
-                        {
-                            media.map((medium) => {
-                                return (
-                        <Link key={medium.id} to={`/${props.category}/` + medium.id} onClick={onclick}>
-                            <MediaCard media={medium} />
-                        </Link>
-                    )})
-                    }
-                </div>
+  return (
+    <section className="sectionMedia">
+      <header>
+        <h1 className="title">{props.title}</h1>
+      </header>
 
+      <div className="linkContainer">
+        <Link className="link" to="/all">
+          <CiGrid41 /> 
+        </Link>
+      </div>
 
-                <div className="buttons">
-                    <div className="buttonsContainer">
-                            <button onClick={clickLeft} className="button"><MdOutlineKeyboardArrowLeft/></button>
-                            <button onClick={clickRight} className="button"><MdOutlineKeyboardArrowRight/> </button>
-                    </div>
-                </div>
+      <section className="sectionMediaContent">
+        <div className="mediaContainer">
+          {media.map((medium) => {
+            return (
+              <Link key={medium.id} to={`/${props.category}/` + medium.id} onClick={onclick}>
+                <MediaCard media={medium} />
+              </Link>
+            );
+          })}
+        </div>
 
-            </section>
-            
-        </section>
-    )
+        <div className="buttons">
+          <div className="buttonsContainer">
+            <button onClick={clickLeft} className="button">
+              <MdOutlineKeyboardArrowLeft />
+            </button>
+            <button onClick={clickRight} className="button">
+              <MdOutlineKeyboardArrowRight />{" "}
+            </button>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
 }
