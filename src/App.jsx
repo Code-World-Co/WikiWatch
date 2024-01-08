d24b3eb4a9fbb801eb7cb69d6a971b399b9d5264
Esnaider.OR <80131310+Darkin03@users.noreply.github.com>
2024-01-07 20:12:57 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,38 @@
-import { useEffect, useState } from "react";
-//import components
-import { Header } from "./components/Header";
-import { Home } from "./components/home";
-import { Movies } from "./components/movies";
-import { Tv } from "./components/tv";
-import { Footer } from "./components/Footer";
-import { Routes, Route } from "react-router-dom";
-import './style/App.css';
-import { MovieInfo } from "./components/movieInfo";
-import  { AlertHome } from "./components/AlertHome";
-import { TvInfo } from "./components/tvInfo";
-import { All } from "./components/All";
+//importando los componentes de navegacion
+import { AboutMe } from "./components/AboutMe";
+import { SkillsSection } from "./sections/SkillsSection";
+import { Prcontenedor } from "./sections/PrContenedor";
+import { Contacts } from "./sections/Contactos";
+import { Header} from "./components/Header";
+import {  Route, Routes } from "react-router-dom";
+import { Footer } from "./components/Footer"; 
+import "./style/style.css";
+import { Up } from "./components/Up";
+
 
 function App() {
 
-  const [valueAlert, setValueAlert] = useState(null)
 
-  useEffect(() => {
-    setValueAlert(window.localStorage.getItem('alert_18'))
-  }, [])
-
-  const alertAdult = () => {
-    setValueAlert('true')
-    window.localStorage.setItem('alert_18', 'true')
-  }
-
-  const alertNoAdult = () => {
-    setValueAlert('false')
-    window.localStorage.setItem('alert_18', 'false')
-  }
 
   return (
-    <div className="App">
-      {
-        valueAlert !== null ?
-          <div>
-            <Header />
-            <Routes>
-              <Route path="/WikiWatch/" element={<Home />} />
-              <Route path="/movies" element={<Movies isAdult={valueAlert} />} />
-              <Route path="/movie/:id" element={<MovieInfo />} />
-              <Route path="/tv/:id" element={<TvInfo />} />
-              <Route path="/tv" element={<Tv isAdult={valueAlert} />} />
-              <Route path="/all/:category/:media" element={<All />} />
+    <>
+      
+      <Header/>
 
-            </Routes>
-            <Footer />
-          </div>
-          :
-          <AlertHome {...{alertAdult, alertNoAdult}} />
-      }
-    </div>
+      <section >
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/habilidades" element={<SkillsSection />} />
+          <Route path="/proyectos" element={<Prcontenedor />} />
+          <Route path="/contactos" element={<Contacts />} />
+        </Routes>
+       
+      </section>
+
+      <Footer/>
+     
+      <Up/>
+    </>
   );
 }
 
